c95599d66a77deb96a12b30e798116794bfa5e15
Alan Lau <alanw.lau@mail.utoronto.ca>
2023-11-21 22:04:49 -0500

    Add destroy in the main activity to get rid of saved data in the app shared preferences

--- app/src/main/java/com/example/swim/MainActivity.java
+++ app/src/main/java/com/example/swim/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.swim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String KEY_CLICKED_LATLNG = "clicked_latlng";
+    private static final String PREF_NAME = "MyPrefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
 
+        // Remove or clear the clickedLatLng related preferences
+        clearClickedLatLngPreferences();
+    }
+
+    private void clearClickedLatLngPreferences() {
+        SharedPreferences prefs = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.remove(KEY_CLICKED_LATLNG + "_LAT");
+        editor.remove(KEY_CLICKED_LATLNG + "_LNG");
+        editor.apply();
+    }
 
 }
