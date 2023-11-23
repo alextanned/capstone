07a04fdc45dbc2c5b508cfcb68131daaf1785780
Alex Tan <alexshiqi.tan@mail.utoronto.ca>
2023-11-23 03:53:17 -0500

    stillllllll working on compass

--- app/src/main/java/com/example/swim/CompassActivity.java
+++ app/src/main/java/com/example/swim/CompassActivity.java
@@ -42,6 +42,8 @@ public class CompassActivity extends AppCompatActivity {
 
     private ExecutorService executorService = Executors.newSingleThreadExecutor();
     private Socket socket = null;
+    static final float ALPHA = 0.25f;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +63,7 @@ public class CompassActivity extends AppCompatActivity {
         }else{
             headingTextView.setText("AVAILABLE!");
         }
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
 //        // Initialize magnetic field and accelerometer sensors
 //        magneticFieldSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
@@ -68,7 +71,7 @@ public class CompassActivity extends AppCompatActivity {
 //
 //        // Register sensor listeners
 //        sensorManager.registerListener(sensorEventListener, magneticFieldSensor, SensorManager.SENSOR_DELAY_NORMAL);
-//        sensorManager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
 //        connectToServer();
 
@@ -79,6 +82,9 @@ public class CompassActivity extends AppCompatActivity {
         // Register the sensor listener when the activity is resumed
         if (rotationVectorSensor != null) {
             sensorManager.registerListener(sensorEventListener, rotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (accelerometer != null){
+            sensorManager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
 
@@ -124,6 +130,9 @@ public class CompassActivity extends AppCompatActivity {
     private SensorEventListener sensorEventListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
+            if (event.sensor== accelerometer){
+                accelerometerValues = lowPass(event.values.clone(), accelerometerValues);
+            }
 //            Log.d("accuracy", String.valueOf(accuracy));
             if (event.sensor == rotationVectorSensor) {
 
@@ -149,7 +158,7 @@ public class CompassActivity extends AppCompatActivity {
         // Calculate the azimuth angle (in radians) and convert it to degrees
         float azimuthInRadians = orientationValues[0];
         float azimuthInDegrees = (float) Math.toDegrees(azimuthInRadians);
-        Log.d("pitch", String.valueOf(Math.toDegrees(event.values[1])));
+        Log.d("pitch", String.valueOf(orientationValues[1]));
 //        if(Math.abs(Math.toDegrees(event.values[1])) > 45){
 //            if (Math.toDegrees(event.values[1]) < 0){
 //                event.values[1] += 45;
@@ -185,6 +194,15 @@ public class CompassActivity extends AppCompatActivity {
         directionIndex %= compassDirections.length;
 
         return compassDirections[directionIndex];
+    }
+
+    protected float[] lowPass( float[] input, float[] output ) {
+        if ( output == null ) return input;
+
+        for ( int i=0; i<input.length; i++ ) {
+            output[i] = output[i] + ALPHA * (input[i] - output[i]);
+        }
+        return output;
     }
 
 }
