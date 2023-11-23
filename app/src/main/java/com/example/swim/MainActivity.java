d5ab3fd796d26306b6cafa876adfd815e982c8c8
Alan Lau <alanw.lau@mail.utoronto.ca>
2023-11-22 20:07:10 -0500

    commit to pull

--- app/src/main/java/com/example/swim/MainActivity.java
+++ app/src/main/java/com/example/swim/MainActivity.java
@@ -3,22 +3,15 @@ package com.example.swim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-<<<<<<< HEAD
 import android.content.SharedPreferences;
-=======
->>>>>>> 0a7f7a741a4e11e46b564a9c91d6a32edc4c310d
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-<<<<<<< HEAD
     private static final String KEY_CLICKED_LATLNG = "clicked_latlng";
     private static final String PREF_NAME = "MyPrefs";
 
-=======
->>>>>>> 0a7f7a741a4e11e46b564a9c91d6a32edc4c310d
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,25 +38,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-<<<<<<< HEAD
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // Remove or clear the clickedLatLng related preferences
-        clearClickedLatLngPreferences();
-    }
-
-    private void clearClickedLatLngPreferences() {
-        SharedPreferences prefs = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.remove(KEY_CLICKED_LATLNG + "_LAT");
-        editor.remove(KEY_CLICKED_LATLNG + "_LNG");
-        editor.apply();
-    }
-=======
 
         Button openServerButton = findViewById(R.id.openServerButton);
         openServerButton.setOnClickListener(new View.OnClickListener() {
@@ -85,7 +59,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
 
->>>>>>> 0a7f7a741a4e11e46b564a9c91d6a32edc4c310d
+        // Remove or clear the clickedLatLng related preferences
+        clearClickedLatLngPreferences();
+    }
+
+    private void clearClickedLatLngPreferences() {
+        SharedPreferences prefs = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.remove(KEY_CLICKED_LATLNG + "_LAT");
+        editor.remove(KEY_CLICKED_LATLNG + "_LNG");
+        editor.apply();
+    }
 
 }
